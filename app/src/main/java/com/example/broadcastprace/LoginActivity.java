a9caab2b7b148a7db256729348ed5e150b50dea9
申凡 <shenfan@shenfandeMacBook-Pro.local>
2020-01-31 12:25:46 +0800

    广播

--- app/src/main/java/com/example/broadcastprace/LoginActivity.java
+++ app/src/main/java/com/example/broadcastprace/LoginActivity.java
@@ -5,28 +5,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.broadcastprace.BaseActivity;
 import com.example.broadcastprace.R;
 
+import java.util.prefs.PreferenceChangeEvent;
+
 public class LoginActivity extends BaseActivity {
 
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button login;
+
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private CheckBox rememberPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //获取到SharedPreferences对象
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+
         accountEdit = (EditText) findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
+
+        rememberPass = (CheckBox) findViewById(R.id.remeber_pass);
+
         login = (Button) findViewById(R.id.login);
+
+        //调用pref对象的getBoolean方法去获取remeber_password这个键对应的值
+        //由于刚开始不存在对应的值，所以使用默认值false
+        boolean isRemember = pref.getBoolean("remember_password",false);
+
+        if(isRemember){
+            //将账号和密码都设置到文本框中
+            String account = pref.getString("account","");
+            String password = pref.getString("password","");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +65,17 @@ public class LoginActivity extends BaseActivity {
 
                 //如果帐号是admin，密码是123456就认为登录成功
                 if(account.equals("admin") && password.equals("123456")){
+                    editor = pref.edit();
+                    if(rememberPass.isChecked()){//检查复选框是否被选中
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    }else{
+                        editor.clear();
+                    }
+                        editor.apply();
+
+
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
                     finish();
